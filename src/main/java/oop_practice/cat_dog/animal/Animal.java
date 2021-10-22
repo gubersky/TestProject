476876a6d49045b6f879524be33fd183e27cc8af
autodoc <alexeym@pkwteile.de>
2021-10-22 16:51:09 +0300

    commit

--- src/main/java/oop_practice/cat_dog/animal/Animal.java
+++ src/main/java/oop_practice/cat_dog/animal/Animal.java
@@ -1,5 +1,16 @@
 package oop_practice.cat_dog.animal;
 
-public class Animal {
+public abstract class Animal {
+
+    public String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    protected Animal() {
+    }
+
+    public abstract void sound();
 
 }
--- src/main/java/oop_practice/cat_dog/cat/Cat.java
+++ src/main/java/oop_practice/cat_dog/cat/Cat.java
@@ -1,5 +1,15 @@
 package oop_practice.cat_dog.cat;
 
-public class Cat {
-    //bla
+import oop_practice.cat_dog.animal.Animal;
+
+public class Cat extends Animal {
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("I'am a Cat. My name " + name + ". Mew");
+    }
 }
